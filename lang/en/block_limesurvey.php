325c0139936117b47ba57904e644ce7289a2a221
SergioComeron <sergiocomeron@icloud.com>
2024-11-21 11:55:47 +0100

    Reorganize language strings

--- lang/en/block_limesurvey.php
+++ lang/en/block_limesurvey.php
@@ -1,10 +1,10 @@
 <?php
-$string['pluginname'] = 'LimeSurvey Block';
-$string['api_token'] = 'Token de la API de LimeSurvey';
-$string['api_token_desc'] = 'Introduce el token de la API de LimeSurvey para permitir la integración.';
-$string['api_url'] = 'URL de la API de LimeSurvey';
-$string['api_url_desc'] = 'Introduce la URL de la API de LimeSurvey (por ejemplo, https://your-limesurvey-domain/index.php/admin/remotecontrol).';
-$string['api_user'] = 'Usuario de la API';
-$string['api_user_desc'] = 'Nombre de usuario para conectarse a la API de LimeSurvey.';
-$string['api_password'] = 'Contraseña de la API';
 $string['api_password_desc'] = 'Contraseña para conectarse a la API de LimeSurvey.';
+$string['api_password'] = 'Contraseña de la API';
+$string['api_token_desc'] = 'Introduce el token de la API de LimeSurvey para permitir la integración.';
+$string['api_token'] = 'Token de la API de LimeSurvey';
+$string['api_url_desc'] = 'Introduce la URL de la API de LimeSurvey (por ejemplo, https://your-limesurvey-domain/index.php/admin/remotecontrol).';
+$string['api_url'] = 'URL de la API de LimeSurvey';
+$string['api_user_desc'] = 'Nombre de usuario para conectarse a la API de LimeSurvey.';
+$string['api_user'] = 'Usuario de la API';
+$string['pluginname'] = 'LimeSurvey Block';
